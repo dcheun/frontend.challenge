f6c491e562aef11b35ca6714095a02d0392405dc
Danny Cheun <dcheun@gmail.com>
2021-08-24 12:55:59 -0700

    feat: Filter Agenda Events By Calendar
    Add select menu that will filter events list by calendar selected. By
    default it displays all calendar events.
    
    Ref: Level 3

--- src/components/Agenda/index.tsx
+++ src/components/Agenda/index.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useContext, useMemo } from 'react'
+import React, { ReactElement, useContext, useMemo, useState } from 'react'
 import { DateTime } from 'luxon'
 
 import greeting from 'lib/greeting'
@@ -28,25 +28,45 @@ const compareByDateTime = (a: AgendaItem, b: AgendaItem) =>
 
 const Agenda = (): ReactElement => {
   const account = useContext(AccountContext)
+  const [calChoice, setCalChoice] = useState('all')
 
   const events: AgendaItem[] = useMemo(
     () =>
       account.calendars
+        .filter((calendar) => {
+          return calChoice === 'all' || calChoice === calendar.color
+        })
         .flatMap((calendar) =>
           calendar.events.map((event) => ({ calendar, event })),
         )
         .sort(compareByDateTime),
-    [account],
+    [account, calChoice],
   )
 
   const localHour = DateTime.local().hour
   const title = useMemo(() => greeting(localHour), [localHour])
+
+  const handleCalChange = (e: React.ChangeEvent<HTMLSelectElement>): void => {
+    const { value } = e.target
+    setCalChoice(value)
+  }
 
   return (
     <div className={style.outer}>
       <div className={style.container}>
         <div className={style.header}>
           <span className={style.title}>{title}</span>
+          <span className="dropdown">
+            <label htmlFor="cal">Calendar: </label>
+            <select name="cal" onChange={handleCalChange} defaultValue="all">
+              <option value="all">All</option>
+              {account.calendars.map((c) => (
+                <option key={c.id} value={c.color}>
+                  {c.color}
+                </option>
+              ))}
+            </select>
+          </span>
         </div>
 
         <List>
